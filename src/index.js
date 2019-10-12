686b8f2639849dff54aa8a1641950f0016c19fa6
Volha Soika <volhasoika@gmail.com>
2019-10-12 11:31:40 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function zeros(expression) {
-  // your solution
+
+  let removeMult = expression.split('*');
+  let zeros = [0, 0]; 
+   for (let i = 0; i < removeMult.length; i++) {
+
+    let item = removeMult[i];
+    let integerItem = parseInt(item);
+    
+    let factorial = item.includes('!!') ? 2 : 1;
+
+    while (integerItem > 0) {
+      if (integerItem % 2 === 0) {
+        zeros[0]++;
+      }
+      if (integerItem % 5 === 0) {
+        zeros[1]++;
+      }
+      if (integerItem % 25 ===0) {
+      zeros[1]++;
+    }
+      integerItem -= factorial;
+    }
+  }
+  return Math.min(...zeros);
 }
